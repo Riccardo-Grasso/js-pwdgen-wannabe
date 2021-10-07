aa7f5b53f12d8e2657532d04864497c61b142ba3
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-07 15:41:48 +0200

    prima versione

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const promptNomeUtente = prompt("Inseire nome:")
+console.log("Il nome dell'utente è:", promptNomeUtente)
+
+const promptCognomeUtente = prompt("Inseire Cognome:")
+console.log("Il cognome dell'utente è:", promptCognomeUtente)
+
+const promptColorePreferitoUtente = prompt("Inseire colore preferito:")
+console.log("Il colore preferito dell'utente è:", promptColorePreferitoUtente)
+
+console.log("La password dell'utente è:", promptNomeUtente+promptCognomeUtente+promptColorePreferitoUtente+21)
+alert(`'La vostra password è: ${promptNomeUtente}${promptCognomeUtente}${promptColorePreferitoUtente}21`);
