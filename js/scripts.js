b0a5a902984433c542a47056d0fd0f44d05a7b02
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-07 16:13:47 +0200

    distanziato elementi prompt

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,5 @@ console.log("Il cognome dell'utente è:", promptCognomeUtente)
 const promptColorePreferitoUtente = prompt("Inseire colore preferito:")
 console.log("Il colore preferito dell'utente è:", promptColorePreferitoUtente)
 
-console.log("La password dell'utente è:", promptNomeUtente+promptCognomeUtente+promptColorePreferitoUtente+21)
+console.log("La password dell'utente è:", promptNomeUtente + promptCognomeUtente + promptColorePreferitoUtente + 21)
 alert(`La vostra password è: ${promptNomeUtente}${promptCognomeUtente}${promptColorePreferitoUtente}21`);
